e6e8276a3f72c47b8c275930a46edeba2391499f
AnjaB <221894507+AnjaB4@users.noreply.github.com>
2026-08-28 14:37:23 +0200

    Add new logo to layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -58,7 +58,7 @@ $roleLibrarian = Application::$app->session->isInRole('Librarian');
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0">
-            <img src="../assets/myimg/kofi.jpg" width="36px" height="36px" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="../assets/myimg/books-pixel-logo.jpg" width="36px" height="36px" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">Reading Journal</span>
         </a>
     </div>
